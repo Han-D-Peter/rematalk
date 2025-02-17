ec03a36f609aaeed798bff3aed54ffc1bc5522ae
Han-D-Peter <secret_wings@naver.com>
2025-02-17 14:45:20 +0900

    fix

--- component/ChatRoom.tsx
+++ component/ChatRoom.tsx
@@ -220,6 +220,7 @@ export default function ChatRoom({
             border: "none",
           }}
           multiline
+          inputRef={inputRef}
           variant="standard"
           onChange={onChange}
         />
