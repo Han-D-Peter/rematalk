ca4a7ace3876f7eb7f83dd598a7f9632b31387ab
Han-D-Peter <secret_wings@naver.com>
2025-02-18 16:11:49 +0900

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
       <head>
         <meta
           name="viewport"
-          content="width=device-width, initial-scale=1.0, user-scalable=no"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
         />
       </head>
       <body className={roboto.variable} style={{ margin: "0" }}>
